8b8a36743be4a8957977a5360e81a7096df7ffda
YangJihoon <ysh4267@gmail.com>
2018-01-29 15:51:05 +0900

    1-5 보스 마법사로 교체, 마법사 사망애니메이션 추가, 로고 추가

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour {
+
+    public GameObject[] Spr = new GameObject[4];
+
+    float num = 0;
+    bool isFade = false;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("StartFade", 1f);
+        Invoke("EndLogo", 4f);
+	}
+
+    void Update()
+    {
+        if (num < 255 && isFade) num += 0.2f;
+        Color colorA = new Vector4(255, 255, 255, num);
+        Spr[0].GetComponent<SpriteRenderer>().color = colorA;
+        Spr[1].GetComponent<SpriteRenderer>().color = colorA;
+    }
+
+    void StartFade()
+    {
+        Spr[2].GetComponent<SpriteRenderer>().color = new Vector4(255, 255, 255, 0);
+        Spr[3].GetComponent<SpriteRenderer>().color = new Vector4(255, 255, 255, 0);
+        Debug.Log("start");
+        isFade = true;
+    }
+
+    void EndLogo()
+    {
+        GameManager.instance.FadeAndLoadScene("Title");
+    }
+}
--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-
     // Use this for initialization
     void Start()
     {
